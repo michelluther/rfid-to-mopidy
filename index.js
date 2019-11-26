d2370fe89dc93e1f59c0cbca3156041ce3782c22
michelluther <luther@lutherundwinter.de>
2019-11-26 22:58:33 +0100

    updated script to consider rfid_to_mopidy_url-mapping

--- index.js
+++ index.js
@@ -9,25 +9,30 @@ const mopidyClient = new Mopidy({
 })
 const statusMap = {
     playing: 'PLAYING',
+    launched_play: 'PLAYING_TRIGGERED',
     paused: 'PAUSED'
 }
 let currentlyActiveCard;
 
 let currentStatus;
 
+let rfidTrackMapping = require('./rfid_to_mopidy_url.json')
+
 
 cardDetector.on('card-detected', (card) => {
     if(!currentlyActiveCard || currentlyActiveCard.getId() !== card.getId()){
         console.log('this is the card that just got detected: ' + card.getId())
         currentlyActiveCard = card;
-    }
-    if(currentStatus !== statusMap.playing)
+        playRFIDCard(card)
+    } else if(currentStatus !== statusMap.playing && currentlyActiveCard !== statusMap.launched_play){
+        status = statusMap.launched_play
         mopidyClient.playback.play({"tl_track":null,"tlid":null}).then(function(data){
             currentStatus = statusMap.playing
             console.log('started playing')
         }).catch(reason =>{
             console.log('hey, something went wrong')
-        });
+        })
+    }
 })
 
 cardDetector.on('card-removed', () => {
@@ -37,5 +42,12 @@ cardDetector.on('card-removed', () => {
             console.log('paused playback')
         });
 })
+
+playRFIDCard = (card) => {
+    mopidyClient.playback.stop({})
+        .then(() => {mopidyClient.tracklist.clear({}).then(() => {
+            mopidyClient.tracklist.add({"uris":[rfidTrackMapping[card.getId()].uri]})
+        }) })
+}
 
 cardDetector.scanForCards();
