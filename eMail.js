1b904f0c250a3e45bc7f62b750b5cf2800101f24
michelluther <luther@lutherundwinter.de>
2020-01-04 22:18:20 +0100

    made connection to mopidy client a pre condition to starting the rest

--- eMail.js
+++ eMail.js
@@ -1,4 +1,4 @@
-const nodemailer = require('nodemailer'),
+const nodemailer = require('nodemailer')
 const config = require('./config') 
 
 const sendMessage = async (toAddress, subject, text) => {
--- index.js
+++ index.js
@@ -4,13 +4,29 @@ const email = require('./eMail')
 const config = require('./config')
 
 let cardDetector = null;
+let mopidyClient = null;
+const setUpMopidyClient = () => {
 
-setTimeout(() => {
-    cardDetector = new CardDetector();
-    const mopidyClient = new Mopidy({
-        webSocketUrl: "ws://localhost:6680/mopidy/ws/",
-        callingConvention: "by-position-or-by-name"
+    const returnPromise = new Promise((resolve, reject) => {
+        mopidyClient = new Mopidy({
+            webSocketUrl: "ws://localhost:6680/mopidy/ws/",
+            callingConvention: "by-position-or-by-name",
+            
+        })
+        const onConnected = () => {
+            mopidyClient.removeAllListeners('web')
+            resolve(mopidyClient)
+        }
+        mopidyClient.on('websocket:open', onConnected)
+        mopidyClient.on('websocket:error', () => {
+            reject(mopidyClient)
+        })
     })
+    return returnPromise
+}
+const setUpCardDetector = () => {
+    cardDetector = new CardDetector();
+
     const statusMap = {
         playing: 'PLAYING',
         launched_play: 'PLAYING_TRIGGERED',
@@ -66,24 +82,34 @@ setTimeout(() => {
     }
     
     cardDetector.scanForCards();
-}, 1000)
+}
 
 function exitHandler(options, exitCode) {
     console.log('cleaning up');
-    cardDetector.resetReader();
+    if(cardDetector){
+        cardDetector.resetReader();
+    }
     if (exitCode || exitCode === 0) console.log(exitCode);
     if (options.exit) process.exit();
 }
 
+let intervalId = setInterval(() => {
+    try {
+        setUpMopidyClient().then(() => {
+            setUpCardDetector();
+            clearInterval(intervalId)
+        }).catch((error) => {
+            console.log('connection to mopidy failed, will try again')
+        })
+    } catch (exc) {
+        console.log(exc)
+        console.log('could not connect to mopidy, will try again in 1 second')
+    }   
+},1000)
+
 //do something when app is closing
 process.on('exit', exitHandler.bind(null,{cleanup:true}));
-
-//catches ctrl+c event
 process.on('SIGINT', exitHandler.bind(null, {exit:true}));
-
-// catches "kill pid" (for example: nodemon restart)
 process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
 process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
-
-//catches uncaught exceptions
 process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
